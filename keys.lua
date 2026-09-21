71a60f70c0d9c11e2e3f1fb62e34a35ee113d021
Keem-pastebin <cutarkeem376@gmail.com>
2026-09-21 12:24:40 +0800

    Add files via upload

---
+++ keys.lua
@@ -0,0 +1,38 @@
+{
+  "keys": [
+    {
+      "password": "A9F3-K2LM-7QX4-Z8WP",
+      "mode": "single",
+      "device": "d796771886ca0d06",
+      "expiry": "2026/12/31",
+      "blocked": false
+    },
+{
+      "password": "7DAYSKEY",
+      "mode": "single",
+      "device": "cee78a40f5b2fe24",
+      "expiry": "2026/05/09",
+      "blocked": false
+    },
+    {
+      "password": "Hex",
+      "mode": "single",
+      "device": "0d0fa60a5f3ea450",
+      "expiry": "2026/03/29",
+      "blocked": false
+    }, 
+    {
+      "password": "HexPhantomFreemuim",
+      "mode": "multi",
+      "expiry": "2026/11/01",
+      "blocked": false
+    }, 
+{
+      "password": "3DaysKey",
+      "mode": "single",
+      "device": "35c0bb3de3dc86f7",
+      "expiry": "2026/3/23",
+      "blocked": false
+    }
+  ]
+}
